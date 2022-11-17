21c116865a92b8f851719aaf6240c8278cf9a403
Ryan <Ryan@Ryans-Surface.mshome.net>
2022-11-17 12:34:02 -0600

    Finished createnewuser view and created controller for it. Started working on marketplaceview and created a testdata.

---
+++ src/application/controller/CreateNewUserController.java
@@ -0,0 +1,44 @@
+package application.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CreateNewUserController{
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private TextField firstNameInputText;
+
+    @FXML
+    private TextField lastNameInputText;
+
+    @FXML
+    private TextField userNameInputText;
+
+    @FXML
+    private TextField passwordInputText;
+
+    @FXML
+    private TextField confirmPasswordInputText;
+
+    @FXML
+    private Button createAccountButton;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    void backButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createAccountButtonClicked(ActionEvent event) {
+
+    }
+
+}
