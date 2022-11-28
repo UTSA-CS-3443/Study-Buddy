9a02ce2afff4543ddd463d0153fe9372f9629c75
Ryan <Ryan@Ryans-Surface>
2022-11-28 15:21:20 -0600

    Added a button for creating a new study session.

--- src/application/controller/MarketPlaceController.java
+++ src/application/controller/MarketPlaceController.java
@@ -1,18 +1,25 @@
 package application.controller;
+import application.Main;
 import application.model.StudySession;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.util.Callback;
 
 public class MarketPlaceController{
 
+    @FXML
+    private Button createNewSessionButton;
     @FXML
     private ComboBox<String> subjectComboBox;
 
@@ -27,6 +34,26 @@ public class MarketPlaceController{
 
     @FXML
     private Label userNameLabel;
+    
+    @FXML
+    void CreateNewSessionButtonClicked(ActionEvent event) {
+    	try {
+	    	FXMLLoader loader = new FXMLLoader();
+	    	loader.setLocation(Main.class.getResource("./view/CreateSessionView.fxml"));
+	    	CreateNewSessionController controller = new CreateNewSessionController();
+	    	loader.setController(controller);
+	    	
+	    	BorderPane layout = (BorderPane) loader.load();
+	    	
+	    	Scene scene = new Scene(layout);
+	    	Main.stage.setScene(scene);
+	    	Main.stage.setTitle("Create a Session");
+	    	
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    }
+    
     @FXML 
     public void handleMouseClick(MouseEvent arg0) {
     	//if the session is selected, then display a button to go to studysessionview.
